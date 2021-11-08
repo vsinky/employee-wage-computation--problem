b6a068c62ec077bec0ff02c37eb0c6cbbc857926
vsinky <sinkykumariverma@gmail.com>
2021-11-08 21:26:22 +0530

    calculate wages till a condition of total working hours

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -6,12 +6,17 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee Wage Computation");
 		// variables
 		int totalEmpHrs = 0;
-		for (int i = 1; i <= 20; i++) {
+		int totalWorkingDays = 0;
+
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
 			int empHrs = 0;
 			switch (randomNumber) {
@@ -28,8 +33,8 @@ public class EmployeeWage {
 				break;
 			}
 			totalEmpHrs += empHrs;
-			int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage of month :  " + empWage);
 		}
+		int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage of month :  " + empWage);
 	}
 }
