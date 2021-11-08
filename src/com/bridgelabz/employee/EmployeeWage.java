2e8ca1beee4c6de6b30915423652f16370b63b43
vsinky <sinkykumariverma@gmail.com>
2021-11-08 21:21:47 +0530

    calculating wages for a month

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employee;
 
 public class EmployeeWage {
+	// static =class level var = scope within class but based on modifier
 	// constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
@@ -9,24 +10,26 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee Wage Computation");
 		// variables
-		int empHrs = 0;
-		int empWage = 0;
-
-		double randomNumber = Math.floor(Math.random() * 10) % 3;
-		switch ((int) randomNumber) {
-		case IS_FULL_TIME:
-			System.out.println("Employee is present");
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			System.out.println("Employee is present but part time");
-			empHrs = 4;
-			break;
-		default:
-			System.out.println("Employee is absent");
-
+		int totalEmpHrs = 0;
+		for (int i = 1; i <= 20; i++) {
+			int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
+			int empHrs = 0;
+			switch (randomNumber) {
+			case IS_FULL_TIME:
+				System.out.println("Employee is present");
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee is present but part time");
+				empHrs = 4;
+				break;
+			default:
+				System.out.println("Employee is absent");
+				break;
+			}
+			totalEmpHrs += empHrs;
+			int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage of month :  " + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage :  " + empWage);
 	}
 }
