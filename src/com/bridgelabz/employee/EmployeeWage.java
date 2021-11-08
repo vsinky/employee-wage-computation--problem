02cf9a29bebc91df3c33a745f0b188234c99b712
vsinky <sinkykumariverma@gmail.com>
2021-11-08 21:13:11 +0530

    add part time employee & wage

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -4,17 +4,21 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		// constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		// variables
 		int empHrs = 0;
 		int empWage = 0;
 
-		double randomNumber = Math.floor(Math.random() * 10) % 2;
+		double randomNumber = Math.floor(Math.random() * 10) % 3;
 
 		if (IS_FULL_TIME == randomNumber) {
 			System.out.println("Employee is present");
 			empHrs = 8;
+		} else if (IS_PART_TIME == randomNumber) {
+			System.out.println("Employee is present but part time");
+			empHrs = 4;
 		} else {
 			System.out.println("Employee is absent");
 		}
