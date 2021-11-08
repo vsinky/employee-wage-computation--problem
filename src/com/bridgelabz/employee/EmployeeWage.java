af8fcb847e57f7544ea206f1197e4b5b9b8b3c54
vsinky <sinkykumariverma@gmail.com>
2021-11-08 21:17:52 +0530

    solving using case statement

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -1,26 +1,30 @@
 package com.bridgelabz.employee;
 
 public class EmployeeWage {
+	// constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		// constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+		System.out.println("Welcome Employee Wage Computation");
 		// variables
 		int empHrs = 0;
 		int empWage = 0;
 
 		double randomNumber = Math.floor(Math.random() * 10) % 3;
-
-		if (IS_FULL_TIME == randomNumber) {
+		switch ((int) randomNumber) {
+		case IS_FULL_TIME:
 			System.out.println("Employee is present");
 			empHrs = 8;
-		} else if (IS_PART_TIME == randomNumber) {
+			break;
+		case IS_PART_TIME:
 			System.out.println("Employee is present but part time");
 			empHrs = 4;
-		} else {
+			break;
+		default:
 			System.out.println("Employee is absent");
+
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage :  " + empWage);
