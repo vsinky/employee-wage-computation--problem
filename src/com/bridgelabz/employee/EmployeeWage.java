86f714f5808ee10e69e8a843b4fd5dbdabfa2adc
vsinky <sinkykumariverma@gmail.com>
2021-11-09 19:38:48 +0530

    modified: employee wage for multiple companies

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -3,37 +3,35 @@ package com.bridgelabz.employee;
 public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-	public static final int NUM_OF_WORKING_DAYS = 20;
 
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		// variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck) {
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
 			case IS_PART_TIME:
 				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
 				break;
 			default:
 				empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("Day#:  " + totalWorkingDays + " Emp Hr: " + empHrs);
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage:  " + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Company:" + company + "  is:  " + totalEmpWage);
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("DMart", 20, 2, 10);
+		computeEmpWage("Reliance", 10, 4, 20);
 	}
 }
