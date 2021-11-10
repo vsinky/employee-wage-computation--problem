ae6c77e795155002d6075b0c65d1cd75a424556e
vsinky <sinkykumariverma@gmail.com>
2021-11-10 20:04:09 +0530

    Added: total wage for each company

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -4,7 +4,20 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
 		// variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
@@ -25,13 +38,21 @@ public class EmployeeWage {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for Company:" + company + "  is:  " + totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company:  " + company + " is: " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage("DMart", 20, 2, 10);
-		computeEmpWage("Reliance", 10, 4, 20);
+
+		EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
+		EmployeeWage reliance = new EmployeeWage("Reliance", 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 }
